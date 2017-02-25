40993c5656e4fbd4eaf135e658b42bb9e502bb9a
wayzyk <romakorobko@gmail.com>
2017-02-25 16:13:47 +0000

    Change Messages controller and add Messages channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,14 @@
 class MessagesController < ApplicationController
     def create
+        ActionCable.server.broadcast 'messages',
+                                    message: message_params[:body],
+                                    username: cookies.signed[:username]
         head :ok
+    end
+    
+    private
+    
+    def message_params
+        params.require(:message).permit(:body)
     end
 end
